f51b42f25d7ff12eaa003d314f76a1d5ba140218
Javier Artigas <jartigas@isyc.com>
2024-06-20 11:48:11 +0200

    fix: test

--- console-app/src/main/java/org/scrum/psd/battleship/ascii/Main.java
+++ console-app/src/main/java/org/scrum/psd/battleship/ascii/Main.java
@@ -1,11 +1,13 @@
 package org.scrum.psd.battleship.ascii;
 
+import com.microsoft.applicationinsights.core.dependencies.apachecommons.lang3.math.NumberUtils;
 import org.scrum.psd.battleship.controller.GameController;
 import org.scrum.psd.battleship.controller.dto.Letter;
 import org.scrum.psd.battleship.controller.dto.Position;
 import org.scrum.psd.battleship.controller.dto.Ship;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.diogonunes.jcolor.Ansi.colorize;
 import static com.diogonunes.jcolor.Attribute.*;
@@ -73,6 +75,7 @@ public class Main {
         System.out.println("Posicion barcos fallados: " + valores);
       }
       String barcosHundidos = "";
+      String barcosRestantes = "";
       for (Ship ship : enemyFleet) {
         if (ship.getIsSunk()) {
           String positions = "";
@@ -80,7 +83,13 @@ public class Main {
             positions += (String.valueOf(p.getColumn()) + p.getRow() + ",");
           }
           barcosHundidos += ship.getName() + "(" + positions + ")" + "\n";
+        } else {
+          barcosRestantes += ship.getName() + "\n";
         }
+      }
+
+      if (!barcosRestantes.equals("")) {
+        System.out.println("Barcos restantes: " + barcosRestantes);
       }
       if (!barcosHundidos.equals("")) {
         System.out.println("Barcos hundidos: " + barcosHundidos);
@@ -122,17 +131,7 @@ public class Main {
       } else {
         listPlayer1Miss.add(position);
       }
-      Map<String, Boolean> mapToPlayer = new HashMap<>();
-      for (Position lHit : listPlayer1Hit) {
-        String p = lHit.getColumn().name().toUpperCase() + lHit.getRow();
-        mapToPlayer.put(p, lHit.getIsHit());
-      }
-      for (Position lMiss : listPlayer1Miss) {
-        String p = lMiss.getColumn().name().toUpperCase() + lMiss.getRow();
-        mapToPlayer.put(p, lMiss.getIsHit());
-      }
 
-      //System.out.println(GameController.getTable(mapToPlayer));
       System.out.println(isHit ? colorize("Yeah ! Nice hit !", GREEN_TEXT()) : colorize("Miss", CYAN_TEXT()));
       telemetry.trackEvent("Player_ShootPosition", "Position", position.toString(), "IsHit", Boolean.valueOf(isHit).toString());
 
@@ -259,21 +258,125 @@ public class Main {
   private static void InitializeMyFleet() {
     Scanner scanner = new Scanner(System.in);
     myFleet = GameController.initializeShips();
-
     System.out.println("Please position your fleet (Game board has size from A to H and 1 to 8) :");
 
     for (Ship ship : myFleet) {
       System.out.println();
       System.out.printf("Please enter the positions for the %s (size: %s)%n", ship.getName(), ship.getSize());
-      for (int i = 1; i <= ship.getSize(); i++) {
-        System.out.printf("Enter position %s of %s (i.e A3):%n", i, ship.getSize());
 
-        String positionInput = scanner.next();
-        ship.addPosition(positionInput);
-        telemetry.trackEvent("Player_PlaceShipPosition", "Position", positionInput, "Ship", ship.getName(), "PositionInShip", Integer.valueOf(i).toString());
+      for (int i = 1; i <= ship.getSize(); i++) {
+        placePosition(scanner, ship, i, myFleet);
       }
     }
   }
+
+
+  private static void placePosition(Scanner scanner, Ship ship, int i, List<Ship> myFleet) {
+    System.out.printf("Enter position %s of %s (i.e A3):%n", i, ship.getSize());
+
+    String positionInput = scanner.next();
+    if (isPositionValid(positionInput)) {
+      if (!isPositionRepeated(positionInput, ship, myFleet)) {
+        if (isAdjacent(positionInput, ship.getPositions())) {
+          ship.addPosition(positionInput);
+          telemetry.trackEvent("Player_PlaceShipPosition", "Position", positionInput, "Ship", ship.getName(), "PositionInShip", Integer.valueOf(i).toString());
+        } else {
+          System.out.println("Error. Position not available !!");
+          placePosition(scanner, ship, i, myFleet);
+        }
+
+      } else {
+        System.out.println("Error. Position already filled !!");
+        placePosition(scanner, ship, i, myFleet);
+
+      }
+    } else {
+      System.out.println("Error. Out ouf range !!");
+      placePosition(scanner, ship, i, myFleet);
+    }
+  }
+
+  private static boolean isAdjacent(String newPosition1, List<Position> shipPositions) {
+    Position position = new Position(Letter.valueOf(newPosition1.substring(0, 1).toUpperCase().trim()), Integer.parseInt(newPosition1.substring(1, 2)));
+    if (shipPositions.isEmpty()) {
+      return true;
+    }
+
+    Position positionShip1 = shipPositions.get(0);
+
+    if (shipPositions.size() == 1) {
+      boolean adjacentColumn = position.getColumn().ordinal() == positionShip1.getColumn().ordinal() - 1 || position.getColumn().ordinal() == positionShip1.getColumn().ordinal() + 1;
+      boolean adjacentRow = position.getRow() == positionShip1.getRow() - 1 || position.getRow() == positionShip1.getRow() + 1;
+
+      return adjacentColumn || adjacentRow;
+    }
+
+
+    Position positionShip2 = shipPositions.get(1);
+
+    boolean boatIsColumn = positionShip1.getColumn().ordinal() == positionShip2.getColumn().ordinal() - 1 || positionShip1.getColumn().ordinal() == positionShip2.getColumn().ordinal() + 1;
+    boolean boatIsRow = positionShip1.getRow() == positionShip2.getRow() - 1 || positionShip1.getRow() == positionShip2.getRow() + 1;
+
+    if (boatIsColumn) {
+      for (Position boatPosition : shipPositions) {
+        if (position.getRow() == boatPosition.getRow() && (position.getColumn().ordinal() == boatPosition.getColumn().ordinal() - 1 || position.getColumn().ordinal() == boatPosition.getColumn().ordinal() + 1)) {
+          return true;
+        }
+      }
+    } else if (boatIsRow) {
+      for (Position boatPosition : shipPositions) {
+        if ((position.getRow() == boatPosition.getRow() - 1 || position.getRow() == boatPosition.getRow() + 1) && position.getColumn() == boatPosition.getColumn()) {
+          return true;
+        }
+      }
+
+    }
+
+    return false;
+  }
+
+
+  private static boolean isPositionRepeated(String positionInput, Ship ship, List<Ship> myFleet) {
+    List<Position> positionsList = myFleet.stream()
+        .map(Ship::getPositions)
+        .filter(Objects::nonNull)
+        .flatMap(Collection::stream)
+        .collect(Collectors.toList());
+    //positionsList.addAll(ship.getPositions());
+    Position newPosition = new Position(Letter.valueOf(positionInput.substring(0, 1).toUpperCase().trim()), Integer.parseInt(positionInput.substring(1, 2)));
+    return positionsList.stream().filter(p -> p.getRow() == newPosition.getRow() && p.getColumn() == newPosition.getColumn()).collect(Collectors.toList()).size() != 0;
+    /*positionsList.add(newPosition);
+
+    Set<Position> positionsSet = new HashSet<>(positionsList);
+    return positionsSet.size() < positionsList.size();*/
+  }
+
+  private static boolean isPositionValid(String positionInput) {
+    if (positionInput.length() > 2) {
+      return false;
+    }
+    if (NumberUtils.isParsable(positionInput.substring(1, 2))
+        && (Integer.parseInt(positionInput.substring(1, 2)) < 1
+        || (Integer.parseInt(positionInput.substring(1, 2)) > 8))) {
+      return false;
+    }
+    String letter = positionInput.substring(0, 1);
+    return isInEnum(letter, Letter.class);
+  }
+
+  private static boolean isInEnum(String value, Class<Letter> enumClass) {
+    try {
+      for (Letter e : enumClass.getEnumConstants()) {
+        if (e.name().equals(value.toUpperCase().trim())) {
+          return true;
+        }
+      }
+    } catch (IllegalArgumentException e) {
+      return false;
+    }
+    return false;
+  }
+
 
   private static void InitializeEnemyFleet() {
     enemyFleet = GameController.initializeShips();
